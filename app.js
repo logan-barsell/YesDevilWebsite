b79cedd9e23553057f444913d88dd93c5160d634
Logan Barsell <loganbarsell@Logans-MacBook-Pro.local>
2018-06-06 13:50:15 -0700

    attempt2debug502

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const express = require('express'),
 	.use('/api', require('./routes/api').route)
 
 
-http.createServer(app).listen(8080, 'localhost')
+http.createServer(app).listen(8080, 'yesdevil.com')
 
 // app.listen(8080, f=> {
 // console.log('Server Running!')
